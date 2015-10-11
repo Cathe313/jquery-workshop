bd83cda312088154aa934255cc4f5c1b7c51b2d0
Catherine <ducharme.catherine@gmail.com>
2015-10-11 22:37:21 +0000

    Added password validation mechanism to the form

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-// Write your code here!
+
+$('#signup-form').on('submit', function(event) {
+    
+    var pass1 = $("input[name=password]").val();
+    var pass2 = $("input[name=password2]").val();
+    
+    if (pass1 != pass2) {
+        alert("Passwords don't match!");
+    }
+    else if (pass1.length < 10) {
+        alert("Password is too short!");
+    } 
+    else {
+        return;
+    }
+    
+    event.preventDefault();
+});
